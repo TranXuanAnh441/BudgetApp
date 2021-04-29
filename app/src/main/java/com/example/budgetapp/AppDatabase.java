0ada50dd4de8a11ab875dd65fa50a9793315e196
XUAN ANH TRAN <s1f102010052@iniad.org>
2021-04-30 00:11:48 +0900

    auto create category items

--- app/src/main/java/com/example/budgetapp/AppDatabase.java
+++ app/src/main/java/com/example/budgetapp/AppDatabase.java
@@ -1,10 +1,13 @@
 package com.example.budgetapp;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.example.budgetapp.CategoryExpense.CategoryExpenseDao;
 import com.example.budgetapp.CategoryDatabase.Category;
@@ -33,10 +36,33 @@ public abstract class AppDatabase extends RoomDatabase {
     public static synchronized com.example.budgetapp.AppDatabase getInstance(Context context){
         if (instance == null){
             instance = Room.databaseBuilder(context.getApplicationContext(), com.example.budgetapp.AppDatabase.class, "app_database").
-                    fallbackToDestructiveMigration().
+                    fallbackToDestructiveMigration().addCallback(roomCallback).
                     build();
         }
         return instance;
+    }
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private CategoryDao categoryDao;
+        private PopulateDbAsyncTask(AppDatabase db) {
+            categoryDao = db.categoryDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            categoryDao.insert(new Category("Renting"));
+            categoryDao.insert(new Category("Eating"));
+            categoryDao.insert(new Category("Transport"));
+            categoryDao.insert(new Category("Entertainment"));
+            categoryDao.insert(new Category("Health care"));
+            categoryDao.insert(new Category("Education"));
+            return null;
+        }
     }
 
    }
--- app/src/main/java/com/example/budgetapp/Fragments/MonthlyFragment.java
+++ app/src/main/java/com/example/budgetapp/Fragments/MonthlyFragment.java
@@ -1,6 +1,46 @@
 package com.example.budgetapp.Fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+import com.example.budgetapp.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MonthlyFragment extends Fragment {
+    AnyChartView anyChartView;
+    private String[] character = {"A", "B", "C","D"};
+    private Integer[] number = {100,200,300,400};
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.monthly_fragment, container, false);
+        return view;}
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        anyChartView = view.findViewById(R.id.chartView);
+        Pie pie = AnyChart.pie();
+        List<DataEntry> dataEntryList = new ArrayList<>();
+
+        for(int i = 0; i < character.length; i ++){
+            dataEntryList.add(new ValueDataEntry(character[i], number[i]));
+        }
+        pie.data(dataEntryList);
+        anyChartView.setChart(pie);
+    }
 }
